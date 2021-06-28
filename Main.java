66ca9eb922df15ee45514cf30da74eb35b8a7c11
shouyuOC <shouyuoic@gmail.com>
2021-06-28 18:28:12 +0900

    Mainの修正

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Player();
-    monster.showDeck();
+    // Monster monster = new Monster();
     // System.out.println(monster.toString());
+
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
